da7f7b1664bbec08672c0efa604069abab5e0499
lorenzodemicheli <lorenzo.demicheli@bbv.ch>
2018-08-21 10:12:16 +0200

    first checkin

--- src/main/java/sandbox/ingestor/temperature/TemperatureIngestorRest.java
+++ src/main/java/sandbox/ingestor/temperature/TemperatureIngestorRest.java
@@ -2,6 +2,7 @@ package sandbox.ingestor.temperature;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,17 @@ import javax.ws.rs.core.Response;
 @RestController
 public class TemperatureIngestorRest {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceid;
 
     @Autowired
     TemperatureSender sender;
 
-
     @PostMapping(path = "/tempmeasure")
     public Response ingestTemperature(@RequestBody TemperatureMeasure measure) {
+        //TODO move to common package with interceptor
+        System.err.println("Temp Measure instance: "+instanceid);
+
 
         sender.send(measure);
 
@@ -26,4 +31,6 @@ public class TemperatureIngestorRest {
 
 
     }
+
+
 }
